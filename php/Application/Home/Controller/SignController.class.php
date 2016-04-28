9348c77cb14a99b58f29b442c1650139d6cc0026
edire <az8641683@163.com>
2016-04-28 08:19:02 +0800

    删除用户 3000米

--- php/Application/Home/Controller/SignController.class.php
+++ php/Application/Home/Controller/SignController.class.php
@@ -23,7 +23,7 @@ class SignController extends BaseController {
 
         $distance = getDistance($callInfo['latitude'], $callInfo['longitude'], $latitude, $longitude);
 
-        if ($distance > 500) {
+        if ($distance > 3000) {
             ajax_return(null, -1, '距离太远，或定位错误！');
         }
 
